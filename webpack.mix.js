249853c52f5555a4f622d3f406b404f949c133fd
Flavio Hernandes <hernandes.flavio@gmail.com>
2021-06-28 11:00:42 -0300

    Update packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.sass('resources/sass/admin/app.scss', 'public/css/admin');
+mix.sass('resources/admin/sass/app.scss', 'public/css/admin');
 
-mix.js('resources/js/web/app.js', 'public/js/web')
-   .sass('resources/sass/web/app.scss', 'public/css/web');
+mix.js('resources/web/js/app.js', 'public/js/web')
+   .sass('resources/web/sass/app.scss', 'public/css/web');
